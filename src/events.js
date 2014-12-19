3e2096997484aa84bfd580eb447493eb97b8ef01
kiri <satoshi_ebihara@xica.net>
2014-12-19 07:34:52 +0000

    remove temporary debug print and add configurable debug print. fix bug.

--- src/events.js
+++ src/events.js
@@ -3,14 +3,19 @@
 var Events = function Events(properties) {
 
     var self = this,
-        events = (properties.events || self.events) || {};
+        events = (properties.events || self.events) || {},
+        debug = properties.debug || false;
 
     self._unsubscriber = new Bacon.Bus();
 
     self._unsubscriber.onValue(function() {
         delete self.ev;
-        self.ev = _.mapValues(events, function(eventThunk) {
-            return eventThunk.call(self).takeUntil(self._unsubscriber);
+        self.ev = _.mapValues(events, function(thunk, name) {
+            var stream = thunk.call(self).takeUntil(self._unsubscriber);
+            if (debug) {
+                stream.log('organon.events.' + (properties.name ? properties.name + '.' : '') + name);
+            }
+            return stream;
         });
     });
 };
